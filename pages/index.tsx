9094b3b8df5c4c7a714d824eecd37de6dcabadde
Alice2212 <hezealice@gmail.com>
2023-04-18 20:43:02 +0100

    styling home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Navbar } from "@/components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
+import classes from "/..styles/home.module.css"
 import { BsTwitter, BsGithub, BsLinkedin, BsDownload } from "react-icons/bs";
 
 export default function Home() {
@@ -14,56 +15,67 @@ export default function Home() {
       <Navbar />
 
       {/* home */}
-      <section className="">
-        {/* socials */}
-        <div className="socials">
-          {/* twitter link */}
-          <Link href="https://twitter.com/AliceOyekale" passHref>
-            <a href="https://twitter.com/AliceOyekale" target="_blank">
-              <BsTwitter />
-            </a>
-          </Link>
+      <section className="container ">
+        <div className="{classes.home} flex top-8 relative m-auto ">
+          {/* socials */}
+          <div className="socials">
+            {/* twitter link */}
+            <Link 
+              href="https://twitter.com/AliceOyekale"
+              target="_blank"
+              passHref
+            >
+              <a href="https://twitter.com/AliceOyekale" target="_blank">
+                <BsTwitter />
+              </a>
+            </Link>
 
-          {/* github */}
-          <Link href="https://github.com/Alice2212" passHref>
-            <a href="https://github.com/Alice2212" target="_blank">
-              <BsGithub />
-            </a>
-          </Link>
+            {/* github */}
+            <Link href="https://github.com/Alice2212" passHref>
+              <a href="https://github.com/Alice2212" target="_blank">
+                <BsGithub />
+              </a>
+            </Link>
 
-          {/* github */}
-          <Link href="https:linkedin.com/in/oyekalealice" passHref>
-            <a href="https:linkedin.com/in/oyekalealice" target="_blank">
-              <BsLinkedin />
-            </a>
-          </Link>
-        </div>
-
-        {/* Introduction */}
-        <div className="content">
-          <h1>Frontend Software Engineer</h1>
-          <h3>
-            Hi, I'm <span className="text-rose-700">Alice</span>
-          </h3>
-          <a
-            href="https://drive.google.com/file/d/1YblmMDW4tZs6tKt_V1DVDPaaOYvwmEe3/view?usp=share_link"
-            target="_blank"
-            className="btn btn-primary"
-          >
-            Download Resume <BsDownload />
-          </a>
-        </div>
-
-        {/* div class circle */}
-        <div className="profile-area">
-          <div className="outer-circle">
-            <span className="material-icons-sharp">design_services</span>
-            <span className="material-icons-sharp">code</span>
-            <span className="material-icons-sharp">code_off</span>
-            <span className="material-icons-sharp">design_services</span>
+            {/* github */}
+            <Link href="https:linkedin.com/in/oyekalealice" passHref>
+              <a href="https:linkedin.com/in/oyekalealice" target="_blank">
+                <BsLinkedin />
+              </a>
+            </Link>
           </div>
-          <div className="inner-circle">
-            <Image src={"/images/alice.jpg"} alt="" width={200} height={200} />
+
+          {/* Introduction */}
+          <div className="content">
+            <h1>Frontend Software Engineer</h1>
+            <h3>
+              Hi, I'm <span className="text-rose-700">Alice</span>
+            </h3>
+            <a
+              href="https://drive.google.com/file/d/1YblmMDW4tZs6tKt_V1DVDPaaOYvwmEe3/view?usp=share_link"
+              target="_blank"
+              className="btn btn-primary"
+            >
+              Download Resume <BsDownload />
+            </a>
+          </div>
+
+          {/* div class circle */}
+          <div className="profile-area">
+            <div className="outer-circle">
+              <span className="material-icons-sharp">design_services</span>
+              <span className="material-icons-sharp">code</span>
+              <span className="material-icons-sharp">code_off</span>
+              <span className="material-icons-sharp">design_services</span>
+            </div>
+            <div className="inner-circle">
+              <Image
+                src={"/images/alice.jpg"}
+                alt=""
+                width={200}
+                height={200}
+              />
+            </div>
           </div>
         </div>
       </section>
