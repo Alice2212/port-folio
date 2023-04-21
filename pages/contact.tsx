0f4137d610ae025bc178ce7ef5640cc0ac38c1a0
Alice2212 <hezealice@gmail.com>
2023-04-21 23:41:03 +0100

    contact section

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,5 @@
+import { AiOutlineMail } from "react-icons/ai";
+
 export default function Contact() {
   return (
     <section>
@@ -19,10 +21,29 @@ export default function Contact() {
                   Get in touch, let's talk
                 </h1>
               </header>
-            {/* contact info */}
-            
+              {/* contact info */}
+              <div className="inline-flex flex-col my-20">
+                <div className="flex flex-row item-center space-x-6 rounded-md border border-[#e5e6f5] hover:border hover:border-blue-500 p-4">
+                  <p className="text-gray-50 font-light text-md ">
+                    <AiOutlineMail />{" "}
+                    <a href="hezealice@gmail.com">hezealice@gmail.com</a>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+
+          <form className="form rounded-lg bg-white p-4 flex flex-col">
+            <label htmlFor="name" className="text-sm text-gray-600 mx-4">
+              
+              Your Name
+            </label>
+            <input
+              type="text"
+              className="font-light rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
+              name="name"
+            />
+          </form>
         </div>
       </div>
     </section>
