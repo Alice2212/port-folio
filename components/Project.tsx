5e07639875c09217ad99e1773909c67a1e7bf980
Alice2212 <hezealice@gmail.com>
2023-06-02 21:30:38 +0100

    added projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import ht from "../public/images/pr1.png";
+import advice from "../public/images/advice.png";
+import edusiteImg from "../public/images/edusite.png";
 import { BsGithub, BsEye } from "react-icons/bs";
 
 const Project = () => {
@@ -19,73 +20,74 @@ const Project = () => {
           data-aos="fade-up"
           className=" flex flex-col justify-center items-center"
         >
-          <div className="flex  flex-col sm:grid sm:grid-cols-3 gap-6">
-            <div className="relative icon-card">
-              <Image
-                src={ht}
-                alt="furniture"
-                className="w-[400px] h-[200px]"
-                width={200}
-                height={200}
-              />
-              <span className="absolute bg-[#ffffff] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4">
-                <h1 className="text-[26px]">Weather</h1>
-                <h1 className="text-[#a456b9]">React.JS/Tailwind Css</h1>
-                <a
-                  href="https://emwesto-website.vercel.app/"
-                  target="_blank"
-                  rel={"noreferrer"}
-                  className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff]"
-                >
-                  See More
-                </a>
-              </span>
-            </div>
-            <div className="relative">
-              <Image
-                src={ht}
-                alt="furniture"
-                className="w-[400px] h-[200px]"
-                width={200}
-                height={200}
-              />
-              <span className="absolute bg-[#ffffff] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4">
-                <h1 className="text-[26px]">Weather</h1>
-                <h1 className="text-[#a456b9]">React.JS/Tailwind Css</h1>
-                <a
-                  href="https://emwesto-website.vercel.app/"
-                  target="_blank"
-                  rel={"noreferrer"}
-                  className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff]"
-                >
-                  See More
-                </a>
-              </span>
-            </div>
-            <div className="icon-card">
+          <div className="grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-1 gap-6">
+            {/* card 1 */}
+            <div className="icon-card ">
               <div className="relative ">
                 <Image
-                  src={ht}
-                  alt="furniture"
-                  className="w-[400px] h-[200px]"
-                  width={200}
-                  height={200}
+                  src={edusiteImg}
+                  alt="education landing page"
+                  className="pb-6"
+                  width={600}
+                  height={400}
                 />
-                <span className="absolute bg-[#9b9696] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4">
-                  <a
-                    href=" "
-                    target="_blank"
-                    rel={"noreferrer"}
-                    className=" px-8 p4-2  hover:text-[#ffffff]"
-                  >
-                    <BsGithub size={34} className="py-2" />
-                    <BsGithub size={34} className="py-2" />
-                  </a>
-                </span>
+                <span className="absolute bg-[#9e9c9c] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4"></span>
               </div>
               <div className="text-center">
-                <h1 className="text-[26px]">Weather</h1>
-                <h1 className="text-[#a456b9]">React.JS/Tailwind Css</h1>
+                <p className="text-[14px] py-2">
+                  Edusite ia a responsive education landing page built with
+                  react and tailwind
+                </p>
+                <div className=" flex flex-wrap justify-center items-center text-center gap-y-2  gap-5 lg:gap-2 mx-auto">
+                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff]  cursor-pointer">
+                    <a href="https://edusite-alice.vercel.app/" target="_blank">
+                      live Demo
+                    </a>
+                  </span>
+                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff] cursor-pointer">
+                    <a
+                      href="https://github.com/Alice2212/edusite.git"
+                      target="_blank"
+                    >
+                      Github
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            {/* card 2 */}
+            <div className="icon-card ">
+              <div className="relative ">
+                <Image
+                  src={edusiteImg}
+                  alt="education landing page"
+                  className="pb-6"
+                  width={600}
+                  height={400}
+                />
+                <span className="absolute bg-[#9e9c9c] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4"></span>
+              </div>
+              <div className="text-center">
+                <p className="text-[14px] py-2">
+                  Edusite ia a responsive education landing page built with
+                  react and tailwind
+                </p>
+                <div className=" flex flex-wrap justify-center items-center text-center gap-y-2  gap-5 lg:gap-2 mx-auto">
+                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff]  cursor-pointer">
+                    <a href="https://edusite-alice.vercel.app/" target="_blank">
+                      live Demo
+                    </a>
+                  </span>
+                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff] cursor-pointer">
+                    <a
+                      href="https://github.com/Alice2212/edusite.git"
+                      target="_blank"
+                    >
+                      Github
+                    </a>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
