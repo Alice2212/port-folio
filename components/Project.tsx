45326e90647fc2acbab1147019409c6754d67ba6
Oluwatosin <hezealice@gmail.com>
2023-08-15 18:35:39 +0100

    project section updated

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import adviceImg from "../public/images/advice.png";
 import edusiteImg from "../public/images/edusite.png";
+import hotel from "../public/images/hotel.png";
 import { BsGithub, BsEye } from "react-icons/bs";
 
 const Project = () => {
@@ -21,7 +22,44 @@ const Project = () => {
           className=" flex flex-col justify-center items-center mx-auto"
         >
           <div className="grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-1 gap-8 mx-auto">
-            {/* card 1 */}
+             {/* card 1 */}
+             <div className="icon-card w-full mx-auto grow">
+              <div className="relative flex items-center justify-center">
+                <Image
+                  src={hotel}
+                  alt="random advice"
+                  className="pb-6"
+                  width={600}
+                  height={400}
+                />
+                {/* <span className="absolute bg-[#9e9c9c] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4"></span> */}
+              </div>
+              <div className="text-center">
+                <p className="text-[14px] py-2">
+                  A responsive hotel landing page built with html, css
+                </p>
+                <div className=" flex flex-wrap justify-center items-center text-center gap-y-2  gap-5 lg:gap-2 mx-auto">
+                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff]  cursor-pointer">
+                    <a
+                      href="https://hotel-website-alice2212.vercel.app/"
+                      target="_blank"
+                    >
+                      live Demo
+                    </a>
+                  </span>
+                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff] cursor-pointer">
+                    <a
+                      href="https://github.com/Alice2212/hotel-website"
+                      target="_blank"
+                    >
+                      Github
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            {/* card 2 */}
+
             <div className="icon-card w-full mx-auto grow">
               <div className="relative flex items-center justify-center">
                 <Image
@@ -55,7 +93,7 @@ const Project = () => {
               </div>
             </div>
 
-            {/* card 2 */}
+            {/* card 3 */}
             <div className="icon-card w-full mx-auto grow">
               <div className="relative flex items-center justify-center">
                 <Image
@@ -92,43 +130,7 @@ const Project = () => {
               </div>
             </div>
 
-            {/* card 3 */}
-            <div className="icon-card w-full mx-auto grow">
-              <div className="relative flex items-center justify-center">
-                <Image
-                  src={adviceImg}
-                  alt="random advice"
-                  className="pb-6"
-                  width={600}
-                  height={400}
-                />
-                {/* <span className="absolute bg-[#9e9c9c] top-0 left-0 w-full h-full opacity-0 transition-opacity duration-300 ease-in-out text-gray-600 hover:opacity-80 flex flex-col items-center justify-center gap-4"></span> */}
-              </div>
-              <div className="text-center">
-                <p className="text-[14px] py-2">
-                  A responsive app that generate random quotes of advice built
-                  with html, css,javascript and an API of advice
-                </p>
-                <div className=" flex flex-wrap justify-center items-center text-center gap-y-2  gap-5 lg:gap-2 mx-auto">
-                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff]  cursor-pointer">
-                    <a
-                      href="https://generate-advice-application.netlify.app/"
-                      target="_blank"
-                    >
-                      live Demo
-                    </a>
-                  </span>
-                  <span className="border-2 border-solid border-[#a456b9] px-6 p4-2 hover:bg-[#a456b9] hover:text-[#ffffff] cursor-pointer">
-                    <a
-                      href="https://github.com/Alice2212/Advice-generator-app.git"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </div>
+           
           </div>
         </div>
       </div>
